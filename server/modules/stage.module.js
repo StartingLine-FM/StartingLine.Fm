bcfb26b50fad41a06f2ceef234685a6644d6cc15
User <user@Users-MacBook-Pro.local>
2023-07-14 13:32:25 -0500

    spelling errors and got the module to show ALL plus the stage selected

--- server/modules/stage.module.js
+++ server/modules/stage.module.js
@@ -5,30 +5,18 @@ module.exports = async function fetchBusinessesByStage(stage) {
 
     switch(stage) {
       case "Nascent":
-        queryText = 'SELECT * FROM stage WHERE name = $1';
-        break;
       case "Early Stage":
-        queryText = 'SELECT * FROM stage WHERE name = $1';
-        break;
       case "Startup/Seed":
-        queryText = 'SELECT * FROM stage WHERE name = $1';
-        break;
       case "Growth":
-        queryText = 'SELECT * FROM stage WHERE name = $1';
+        queryText = 'SELECT * FROM stage WHERE name = $1 OR name = \'All\'';
         break;
       default:  // if no or unknown stage is given, or 'All' is specified, fetch all businesses
         queryText = 'SELECT * FROM stage';
     }
 
     try {
-      // If stage is 'All', we don't need to pass any parameters to the query
-      if (stage === 'All' || stage === undefined || ["Nascent", "Early Stage", "Startup/Seed", "Growth"].indexOf(stage) === -1) {
-        const { rows } = await pool.query(queryText);
-        return rows;
-      } else {
-        const { rows } = await pool.query(queryText, [stage]);
-        return rows;
-      }
+      const { rows } = await pool.query(queryText, [stage]);
+      return rows;
     } catch (error) {
       console.error(`Error fetching businesses in stage "${stage}"`, error);
       throw error;
