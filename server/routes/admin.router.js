ee72789b637f235d3639874db1d8973ef1c4a880
User <user@Users-MacBook-Pro.local>
2023-07-17 11:47:32 -0500

    tested and all work without sercurity, so should still work with

--- server/routes/admin.router.js
+++ server/routes/admin.router.js
@@ -14,9 +14,9 @@ const isAdmin = (req, res, next) => {
   };
 
 //Route to (POST), add a resource for the admin
-router.post('/', rejectUnauthenticated ,isAdmin, async (req, res) => {
+router.post('/', rejectUnauthenticated, isAdmin, async (req, res) => {
     const newResource = req.body;
-    const queryText = 'INSERT INTO resource(stage_id, catergory_id, name, image_url, description, website, email, address, linkedin) VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9)';
+    const queryText = 'INSERT INTO resource(stage_id, category_id, name, image_url, description, website, email, address, linkedin) VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9)';
     try {
         await pool.query(queryText, [newResource.stage_id, newResource.category_id, newResource.name, newResource.image_url, newResource.description, newResource.website, newResource.email, newResource.address, newResource.linkedin]);
         res.sendStatus(201);
@@ -28,7 +28,7 @@ router.post('/', rejectUnauthenticated ,isAdmin, async (req, res) => {
 //Route to (PUT), update a resource for the admin
 router.put('/:id', rejectUnauthenticated, isAdmin, async (req,res) => {
     const updatedResource = req.body;
-    const queryText = 'UPDATE resource SET stage_id = $1, catergory_id = $2, name = $3, image_url = $4, description = $5, website = $6, email = $7, address = $8, linkedin = S9 WHERE id = $10';
+    const queryText = 'UPDATE resource SET stage_id = $1, category_id = $2, name = $3, image_url = $4, description = $5, website = $6, email = $7, address = $8, linkedin = $9 WHERE id = $10';
     try {
         await pool.query(queryText, [updatedResource.stage_id, updatedResource.category_id, updatedResource.name, updatedResource.image_url, updatedResource.description, updatedResource.website, updatedResource.email, updatedResource.address, updatedResource.linkedin, req.params.id]);
         res.sendStatus(200);
