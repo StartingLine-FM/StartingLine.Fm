e2feca92d7c84c20d994d80c5f8d627c12fe17f3
User <user@Users-MacBook-Pro.local>
2023-07-17 10:02:30 -0500

    setup for admin router

---
+++ server/routes/admin.router.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+const pool = require('../modules/pool'); 
+const {
+  rejectUnauthenticated,
+} = require('../modules/authentication-middleware');
+
+//Route to (POST), add a resource for the admin
+
+//Route to (PUT), update a resource for the admin
+
+//Route to (DELETE), a resource for the admin
+
+
+
+module.exports = router;
