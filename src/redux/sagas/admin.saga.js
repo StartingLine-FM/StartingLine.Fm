51111fda4b6991cc3ae1d11f03492ce1d95d2429
User <user@Users-MacBook-Pro.local>
2023-07-18 13:27:14 -0500

    sagas changed names for clarity and code comments

--- src/redux/sagas/admin.saga.js
+++ src/redux/sagas/admin.saga.js
@@ -2,7 +2,7 @@ import {call, put, takeLatest} from 'redux-saga/effects';
 import axios from 'axios';
 
 //Worker Saga: fired when adding a resource
-function* addResource(action) {
+function* postResource(action) {
     try {
         yield call(axios.post, '/api/admin', action.payload);
         yield put({ type: 'FETCH_RESOURCES'});
@@ -11,7 +11,8 @@ function* addResource(action) {
     }
 };
 
-function* updateResource(action) {
+//Worker Saga: fired when a resource is updated
+function* putResource(action) {
     try {
         yield call(axios.put, '/api/admin/${action.payload.id}', action.payload);
         yield put({ type: 'FETCH_RESOURCES'});
@@ -20,6 +21,7 @@ function* updateResource(action) {
     }
 };
 
+//Worker Saga: fired with a resource is deleted 
 function* deleteResource(action) {
     try {
         yield call(axios.delete, '/api/admin/{action.payload');
@@ -30,7 +32,9 @@ function* deleteResource(action) {
 };
 
 function* adminSaga() {
-    yield takeLatest('ADD_RESOURCE', addResource);
-    yield takeLatest('UPDATE_RESOURCE', updateResource);
+    yield takeLatest('POST_RESOURCE', postResource);
+    yield takeLatest('UPDATE_RESOURCE', putResource);
     yield takeLatest('DELETE_RESOURCE', deleteResource);
 }
+
+export default adminSaga;
