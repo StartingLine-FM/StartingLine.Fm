aba6f79baeb9560dd343c0bbf9edb8c15659b720
User <user@Users-MacBook-Pro.local>
2023-07-19 10:54:20 -0500

    menu sidebar has buttons to test getting events and sagas to help fetch them

--- src/components/Pages/CalendarPage/MenuSidebar.jsx
+++ src/components/Pages/CalendarPage/MenuSidebar.jsx
@@ -6,29 +6,35 @@ import dayGridPlugin from '@fullcalendar/daygrid' // a plugin!
 
 
 const CalendarMenu = () => {
+    const EP_Events = useSelector((store) => store.EP.events);
+    const FU_Events = useSelector((store) => store.FU.events);
+    const CHAMBER_Events = useSelector((store) => store.CHAMBER.events);
+    const dispatch = useDispatch();
+  
+    const handleEP_Click = () => {
+      // Fetch Emerging Prairies Calendar Data
+      dispatch({ type: 'FETCH_EP' });
+    };
+  
+    const handleFU_Click = () => {
+      // Fetch Fargo Underground Calendar Data
+      dispatch({ type: 'FETCH_FU' });
+    };
+  
+    const handleChamber_Click = () => {
+      // Fetch Chamber of Commerce Calendar Data
+      dispatch({ type: 'FETCH_CHAMBER' });
+    };
+  
+    return (
+      <div className='button-container'>
+        {/* Dashboard to Call Calendar Data */}
+        <button className='calendar-call' onClick={handleEP_Click}>Emerging Prairie</button>
+        <button className='calendar-call' onClick={handleFU_Click}>Fargo Underground</button>
+        <button className='calendar-call' onClick={handleChamber_Click}>Chamber of Commerce</button>
 
-const EP_Events = useSelector(store => EP.events);
-const FU_Events = useSelector(store => FU.events);
-const CHAMBER_Events = useSelector(store => CHAMBER.events);
-const dispatch = useDispatch();
-
-const handleEP_Click = () => {
-    //Fetch Emerging Prairies Caledndar Data
-    dispatch({ type: 'FETCH_EP' });
-}
-
-return(
-
-<div></div>
-
-
-
-
-
-
-)
-
-
-
-
-}
+      </div>
+    );
+  };
+  
+  export default CalendarMenu;
