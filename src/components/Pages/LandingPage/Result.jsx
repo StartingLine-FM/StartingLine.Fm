ca45854625cb14951551345a8e87b02d5b740ee5
Kord Maurer <kord.r.maurer@gmail.com>
2023-07-23 18:04:00 -0500

    add reference image conditional to Result component

--- src/components/Pages/LandingPage/Result.jsx
+++ src/components/Pages/LandingPage/Result.jsx
@@ -13,8 +13,13 @@ export default function Result({ result }) {
     return (
         <>
             <Card sx={{height: 250}}>
-                <StarBorderIcon />
-                <CardMedia sx={{ height: 100 }} image={result.image && result.image} title='Resource Image' />
+                <StarBorderIcon sx={{m:1}} />
+                <CardMedia 
+                    sx={{ height: 100 }} 
+                    image={result.image 
+                    ? result.image 
+                    : 'https://images.unsplash.com/photo-1595343208792-b7d268abb3be?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1770&q=80'} 
+                    title='Resource Image' />
                 <CardContent>
                     <Typography>
                         {result.name}
