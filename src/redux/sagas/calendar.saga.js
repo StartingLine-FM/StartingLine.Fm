ae5260f25096026b153e859f69ce992a877b92f0
SRJ-91 <Sawyerjen@hotmail.com>
2023-08-02 16:04:22 -0500

    code comments added, scrape files removed, test routers removed

--- src/redux/sagas/calendar.saga.js
+++ src/redux/sagas/calendar.saga.js
@@ -6,11 +6,11 @@ function* fetchEmergingPrairieCalendarSaga() {
   try {
     yield put({ type: 'SET_LOADING_EP'});// set load to true
     const calendarData = yield call(axios.get, '/api/calendar/emerging-prairie');
-    yield put({ type: 'SET_EP', payload: calendarData.data });
+    yield put({ type: 'SET_EP', payload: calendarData.data }); //Sets the EP reducer.
     yield put({ type: 'CLEAR_LOADING_EP'}); // set load to false
   } catch (error) {
     console.log('Error fetching Emerging Prairie calendar:', error);
-    yield put({ type: 'CLEAR_LOADING_EP'}); // set load to false
+    yield put({ type: 'CLEAR_LOADING_EP'}); // set load to false, even if an error occurs
   }
 }
 
@@ -19,11 +19,11 @@ function* fetchFargoUndergroundCalendarSaga() {
   try {
     yield put({ type: 'SET_LOADING_FU'});// set load to true
     const calendarData = yield call(axios.get, '/api/calendar/fargo-underground');
-    yield put({ type: 'SET_FU', payload: calendarData.data });
+    yield put({ type: 'SET_FU', payload: calendarData.data }); //Set the FU recucer
     yield put({ type: 'CLEAR_LOADING_FU'}); // set load to false
   } catch (error) {
     console.log('Error fetching Fargo Underground calendar:', error);
-    yield put({ type: 'CLEAR_LOADING_FU'}); // set load to false
+    yield put({ type: 'CLEAR_LOADING_FU'}); // set load to false, even if an error occurs
   }
 }
 
@@ -32,11 +32,11 @@ function* fetchChamberCalendarSaga() {
   try {
     yield put({ type: 'SET_LOADING_CHAMBER'});// set load to true
     const calendarData = yield call(axios.get, '/api/calendar/chamber');
-    yield put({ type: 'SET_CHAMBER', payload: calendarData.data });
+    yield put({ type: 'SET_CHAMBER', payload: calendarData.data }); //Set the Chamber reducer
     yield put({ type: 'CLEAR_LOADING_CHAMBER'}); // set load to false
   } catch (error) {
     console.log('Error fetching Chamber of Commerce calendar:', error);
-    yield put({ type: 'CLEAR_LOADING_CHAMBER'}); // set load to false
+    yield put({ type: 'CLEAR_LOADING_CHAMBER'}); // set load to false, even if an error occurs
   }
 }
 
