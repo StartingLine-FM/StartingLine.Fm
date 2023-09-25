cd738f6b37b98653abd5f0a4051531f011c5906d
Kord Maurer <kord.r.maurer@gmail.com>
2023-09-25 09:49:58 -0500

    fix login modal closing on error

--- src/components/Pages/LoginPage/LoginPage.jsx
+++ src/components/Pages/LoginPage/LoginPage.jsx
@@ -1,17 +1,22 @@
 import React from 'react';
 import LoginForm from './LoginForm';
 import { useHistory } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 // material ui imports 
-import { Container, Paper, Button } from '@mui/material';
+import { Container } from '@mui/material';
 
 function LoginPage({ handleOpen, openRegisterModal, setOpenRegisterModal, handleCloseRegister, handleOpenRegister, setOpenLoginModal, openLoginModal, handleClose }) {
+
   const history = useHistory();
+  const errors = useSelector(store => store.errors);
+  const user = useSelector(store => store.user);
 
   return (
     // pass in the form component and the props to access the login and register modal
     <Container>
-      <LoginForm handleOpen={handleOpen} openRegisterModal={openRegisterModal} setOpenRegisterModal={setOpenRegisterModal} 
-      handleCloseRegister={handleCloseRegister} handleOpenRegister={handleOpenRegister} openLoginModal={openLoginModal} handleClose={handleClose} setOpenLoginModal={setOpenLoginModal}  />
+      {errors.loginMessage ? handleOpen() : history.push('/home')}
+      <LoginForm handleOpen={handleOpen} openRegisterModal={openRegisterModal} setOpenRegisterModal={setOpenRegisterModal}
+        handleCloseRegister={handleCloseRegister} handleOpenRegister={handleOpenRegister} openLoginModal={openLoginModal} handleClose={handleClose} setOpenLoginModal={setOpenLoginModal} />
     </Container>
   );
 }
