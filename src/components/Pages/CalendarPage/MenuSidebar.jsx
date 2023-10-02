c873ddfe82224455659347fef127851bae419d98
SRJ-91 <Sawyerjen@hotmail.com>
2023-10-02 12:57:27 -0500

    The calendar buttons conditionally render to their colors when active. Also the info tooltip was expanded.

--- src/components/Pages/CalendarPage/MenuSidebar.jsx
+++ src/components/Pages/CalendarPage/MenuSidebar.jsx
@@ -1,22 +1,19 @@
 import React from 'react';
 import "./CalendarPage.css";
 import { useDispatch, useSelector } from 'react-redux';
-import { Box, IconButton, Typography, CircularProgress, Tooltip } from '@mui/material';
-import { Business, BrightnessLow, HorizontalSplit, Clear } from '@mui/icons-material';
-import InfoIcon from '@mui/icons-material/Info';
-
+import { Box, IconButton, Typography, CircularProgress } from '@mui/material';
+import { School, Business, BrightnessLow, HorizontalSplit, Clear } from '@mui/icons-material';
 
 const MenuSidebar = () => {
-
   const dispatch = useDispatch();
 
-  //These reducers hold the response arrays from the calendar.router.js
-  const EP_Events = useSelector((store) => store.EP_Reducer); //Emerging Praire events
-  const FU_Events = useSelector((store) => store.FU_Reducer); //Fargo Underground events
-  const CHAMBER_Events = useSelector((store) => store.chamber_Reducer); //Chamber of Commerce Events
-  const CEFB_Events = useSelector((store) => store.CEFB_Reducer); //NDSU CEFB Events
+  // These reducers hold the response arrays from the calendar.router.js
+  const EP_Events = useSelector((store) => store.EP_Reducer); // Emerging Prairie events
+  const FU_Events = useSelector((store) => store.FU_Reducer); // Fargo Underground events
+  const CHAMBER_Events = useSelector((store) => store.chamber_Reducer); // Chamber of Commerce Events
+  const CEFB_Events = useSelector((store) => store.CEFB_Reducer); // NDSU CEFB Events
 
-  //These reducers manage the load spinners when a calendar is called. 
+  // These reducers manage the load spinners when a calendar is called. 
   const loadingEP = useSelector((state) => state.EP_Loading_Reducer);
   const loadingFU = useSelector((state) => state.FU_Loading_Reducer);
   const loadingChamber = useSelector((state) => state.chamber_Loading_Reducer);
@@ -61,59 +58,60 @@ const MenuSidebar = () => {
       dispatch({ type: 'CLEAR_CEFB' });
     }
   };
-  
 
-  const handleClearClick = () => { //Clears all data from every calendar reducers.
+  const handleClearClick = () => { // Clears all data from every calendar reducers.
     dispatch({ type: 'CLEAR_CALENDARS' });
   };
 
   return (
     <Box>
-
-      <IconButton //The NDSU CEFB Button
-        className='CEFB-button'
+      <IconButton // The NDSU CEFB Button
+        className={`calendar-button`}
         color='inherit'
         onClick={handleCEFB_Click}
-        disabled={loadingCEFB}>
-        {loadingCEFB ? <CircularProgress size={20} /> : <BrightnessLow />}
-        <Typography marginLeft={1}>NDSU CEFB</Typography>
+        disabled={loadingCEFB}
+      >
+        {loadingCEFB ? <CircularProgress size={20} /> : <School color='green'/>}
+        <Typography className={CEFB_Events.length > 0 ? 'active-cefb' : 'passive'} paddingLeft={1} paddingRight={1}>NDSU CEFB</Typography>
       </IconButton>
 
-      <IconButton //The Emerging Prairie Button
-        className='EP-button'
+      <IconButton // The Emerging Prairie Button
+        className={`calendar-button`}
         color='inherit'
         onClick={handleEP_Click}
-        disabled={loadingEP}>
+        disabled={loadingEP}
+      >
         {loadingEP ? <CircularProgress size={20} /> : <BrightnessLow />}
-        <Typography marginLeft={1}>Emerging Prairie</Typography>
+        <Typography className={EP_Events.length > 0 ? 'active-ep' : 'passive'} paddingLeft={1} paddingRight={1}>Emerging Prairie</Typography>
       </IconButton>
 
-      <IconButton //The Fargo Underground Button
-        className='FU-button'
+      <IconButton // The Fargo Underground Button
+        className={`calendar-button`}
         color='inherit'
         onClick={handleFU_Click}
-        disabled={loadingFU}>
+        disabled={loadingFU}
+      >
         {loadingFU ? <CircularProgress size={20} /> : <HorizontalSplit />}
-        <Typography marginLeft={1}>Fargo Underground</Typography>
+        <Typography className={FU_Events.length > 0 ? 'active-fu' : 'passive'} paddingLeft={1} paddingRight={1}>Fargo Underground</Typography>
       </IconButton>
 
-      <IconButton //The Chamber of Commerce Button
-        className='chamber-button'
+      <IconButton // The Chamber of Commerce Button
+        className={`calendar-button`}
         color='inherit'
         onClick={handleChamber_Click}
-        disabled={loadingChamber}>
+        disabled={loadingChamber}
+      >
         {loadingChamber ? <CircularProgress size={20} /> : <Business />}
-        <Typography marginLeft={1}>Chamber of Commerce</Typography>
+        <Typography className={CHAMBER_Events.length > 0 ? 'active-chamber' : 'passive'} paddingLeft={1} paddingRight={1}>FMWF Chamber</Typography>
       </IconButton>
 
-      <IconButton onClick={handleClearClick} //The Clear Button
-        className='clear-button'
+      <IconButton onClick={handleClearClick} // The Clear Button
+        className='calendar-button'
         color='inherit'
       >
         <Clear />
         <Typography marginLeft={1}>Clear Calendars</Typography>
       </IconButton>
-
     </Box>
   );
 };
