8ff13ba79940cde2efbad4c0cf777049dc47bd45
Kord Maurer <kord.r.maurer@gmail.com>
2023-10-03 11:28:39 -0500

    add admin validation to POST, DELETE, & PUT requests, run successful Postman tests with anon user, logged in user, and admin user.

--- server/routes/article.router.js
+++ server/routes/article.router.js
@@ -3,16 +3,16 @@ const pool = require('../modules/pool');
 const router = express.Router();
 const rejectUnauthenticated = require('../modules/authentication-middleware')
 
-// //middleware that checks if the user has admin privileges 
-// const isAdmin = (req, res, next) => {
-//     // If the user is authenticated and has admin privileges, continue with request
-//     if (req.user && req.user.admin) {
-//         next();
-//     } else {
-//         // If user doesn't have admin privileges, send an error response
-//         res.status(403).json({ message: 'You do not have admin access.' });
-//     }
-// };
+//middleware that checks if the user has admin privileges 
+const isAdmin = (req, res, next) => {
+    // If the user is authenticated and has admin privileges, continue with request
+    if (req.user && req.user.admin) {
+        next();
+    } else {
+        // If user doesn't have admin privileges, send an error response
+        res.status(403).json({ message: 'You do not have admin access.' });
+    }
+};
 
 // GET req for all articles
 router.get('/', (req, res) => {
@@ -37,7 +37,7 @@ router.get('/:id', (req, res) => {
 });
 
 // POST req for admin to post new article
-router.post('/', (req, res) => {
+router.post('/', isAdmin, (req, res) => {
     let queryText = `INSERT INTO "article"("title","author","body","image_url")
     VALUES($1, $2, $3, $4)`
     pool.query(queryText, [req.body.title, req.body.author, req.body.body, req.body.image_url])
@@ -49,7 +49,7 @@ router.post('/', (req, res) => {
 });
 
 // DELETE req for admin to delete article
-router.delete('/:id', (req, res) => {
+router.delete('/:id', isAdmin, (req, res) => {
     let queryText = `DELETE FROM "article" WHERE "id" = $1;`
     pool.query(queryText, [req.params.id])
         .then(result => res.sendStatus(204))
@@ -60,7 +60,7 @@ router.delete('/:id', (req, res) => {
 });
 
 // POST req for admin to post new article
-router.put('/:id', (req, res) => {
+router.put('/:id', isAdmin, (req, res) => {
     let queryText = `UPDATE "article" SET "title"=$1,"author"=$2,"body"=$3,"image_url"=$4 WHERE "id" = $5`
     pool.query(queryText, [req.body.title, req.body.author, req.body.body, req.body.image_url, req.params.id])
         .then(result => res.sendStatus(200))
