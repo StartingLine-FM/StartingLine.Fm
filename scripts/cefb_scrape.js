e2854d4bebc9e1a25b0535985e422f6b5dfc7325
SRJ-91 <Sawyerjen@hotmail.com>
2023-10-04 13:54:56 -0500

    Almost done with the cefb scrape script

---
+++ scripts/cefb_scrape.js
@@ -0,0 +1,98 @@
+const axios = require("axios");
+const cheerio = require("cheerio");
+const moment = require("moment");
+const pool = require("../server/modules/pool");
+
+// URL of the CEFB calendar
+const CEFB_url = "https://ndsu-cefb.com/events/list/";
+
+axios
+  .get(CEFB_url)
+  .then(async (response) => {
+    const $ = cheerio.load(response.data);
+    const CEFB_events = [];
+
+    // Loop through each event element and scrape data
+    $(".tribe-events-calendar-list__event-details.tribe-common-g-col").each(
+      (index, element) => {
+        // Logging to check if each element is being processed
+        console.log("Processing element #" + index);
+
+        // Scrape data from the CEFB website
+        const eventHeader = $(element)
+          .find(
+            ".tribe-events-calendar-list__event-title.tribe-common-h6.tribe-common-h4--min-medium"
+          )
+          .text()
+          .replace(/\s+/g, " ");
+
+        // Get the "<time />" element that is always there
+        const startingTimeElement = $(element)
+          .find("time.tribe-events-calendar-list__event-datetime");
+        // Read the date from the datetime attribute
+        const startingDate = startingTimeElement.attr("datetime");
+        // The starting time always comes after an "@" symbol, as the text in the element looks like "October 20 @ 4:00 pm - 6:00 pm," but isn't always there
+        let startingTimeString = startingTimeElement.text().split("@")[1];
+        const startingTime = startingTimeString
+          ? startingTimeString.split("-")[0].trim()
+          : "8:00 am";
+        // Get the ending time if there is one
+        const endingTimeExists = startingTimeString
+          ? startingTimeString.split("-")[1]
+          : false;
+        const endingTime = endingTimeExists
+          ? startingTimeString.split("-")[1].trim()
+          : "5:00 pm";
+
+        // Combine the date and time into a single string
+        const eventStartMoment = moment(
+          `${startingDate} - ${startingTime}`,
+          "YYYY-MM-DD - h:mm a"
+        );
+        const eventStart = eventStartMoment.format("YYYY-MM-DDTHH:mm:ss");
+        const eventEnd = moment(
+          `${startingDate} - ${endingTime}`,
+          "YYYY-MM-DD - h:mm a"
+        ).format("YYYY-MM-DDTHH:mm:ss");
+
+        const eventDescription = $(element)
+          .find("p")
+          .text()
+          .replace(/\s+/g, " ");
+
+        // Logging to check extracted data
+        console.log("Event Header:", eventHeader);
+        console.log("Event Start:", eventStart);
+        console.log("Event End:", eventEnd);
+        console.log("Event Description:", eventDescription);
+
+        // Create an object for the event
+        const CEFB_CalendarBlock = {
+          source: "cefb",
+          title: eventHeader,
+          start: eventStart,
+          end: eventEnd,
+          description: eventDescription,
+        };
+
+        // Add the event object to an array
+        CEFB_events.push(CEFB_CalendarBlock);
+      }
+    );
+
+    // Logging to check the scraped events
+    console.log("CEFB Events:", CEFB_events);
+
+    // Insert all events into the database
+    for (const event of CEFB_events) {
+      await pool.query(
+        `INSERT INTO "calendar" ("source", "title", "start", "end", "description")
+            VALUES ($1, $2, $3, $4, $5);`,
+        [event.source, event.title, event.start, event.end, event.description]
+      );
+    }
+    console.log("Successfully inserted CEFB events into database");
+  })
+  .catch((error) => {
+    console.error("Error scraping CEFB calendar:", error);
+  });
--- server/routes/calendar.router.js
+++ server/routes/calendar.router.js
@@ -4,6 +4,7 @@ const axios = require("axios");
 const cheerio = require("cheerio");
 const moment = require("moment");
 const chrono = require("chrono-node");
+const pool = require("../modules/pool");
 
 //These are the urls of the public calendars being scraped.
 const EP_url = "https://www.emergingprairie.com/calendar/"; //Emerging Praire 
@@ -313,7 +314,6 @@ router.get("/cefb", (req, res) => {
 
           // Create an object for the event
           const CEFB_CalendarBlock = {
-            date: 'some date',
             title: eventHeader,
             start: eventStart,
             end: eventStartMoment.add(1, "hours").format("YYYY-MM-DDTHH:mm:ss.000"),
@@ -337,33 +337,99 @@ router.get("/cefb", (req, res) => {
     });
 });
 
-// router.get("/test-cefb", (req, res) => {
-//   axios
-//     .get(CEFB_url)
-//     .then((response) => {
-//       const $ = cheerio.load(response.data);
+router.get("/test-cefb", async (req, res) => {
+  try {
+    const response = await axios.get(CEFB_url);
+    const $ = cheerio.load(response.data);
+    const CEFB_events = [];
 
-//       // Define the CSS selector for the element you want to scrape
-//       const selector = ".tribe-events-calendar-list__event-details.tribe-common-g-col";
+    // Loop through each event element and scrape data
+    $(".tribe-events-calendar-list__event-details.tribe-common-g-col").each(
+      (index, element) => {
+        // Logging to check if each element is being processed
+        console.log("Processing element #" + index);
 
-//       // Scrape data from a single element
-//       const firstElement = $(selector).first();
-//       const eventHeader = firstElement
-//         .find(".tribe-event-time")
-//         .text()
-//         .replace(/\s+/g, " ");
+        // Scrape data from the CEFB website
+        const eventHeader = $(element)
+          .find(
+            ".tribe-events-calendar-list__event-title.tribe-common-h6.tribe-common-h4--min-medium"
+          )
+          .text()
+          .replace(/\s+/g, " ");
 
-//       // Log the extracted data for testing
-//       console.log("Event Header:", eventHeader);
+        // Get the "<time />" element that is always there
+        const startingTimeElement = $(element)
+          .find("time.tribe-events-calendar-list__event-datetime");
+        // Read the date from the datetime attribute
+        const startingDate = startingTimeElement.attr("datetime");
+        // The starting time always comes after an "@" symbol, as the text in the element looks like "October 20 @ 4:00 pm - 6:00 pm," but isn't always there
+        let startingTimeString = startingTimeElement.text().split("@")[1];
+        const startingTime = startingTimeString
+          ? startingTimeString.split("-")[0].trim()
+          : "8:00 am";
+        // Get the ending time if there is one
+        const endingTimeExists = startingTimeString
+          ? startingTimeString.split("-")[1]
+          : false;
+        const endingTime = endingTimeExists
+          ? startingTimeString.split("-")[1].trim()
+          : "5:00 pm";
 
-//       res.send({ eventHeader }); // Send the scraped data as a response
-//     })
-//     .catch((error) => {
-//       // Log and handle errors
-//       console.error("Error scraping CEFB for testing:", error);
-//       res.sendStatus(500);
-//     });
-// });
+        // Combine the date and time into a single string
+        const eventStartMoment = moment(
+          `${startingDate} - ${startingTime}`,
+          "YYYY-MM-DD - h:mm a"
+        );
+        const eventStart = eventStartMoment.format("YYYY-MM-DDTHH:mm:ss");
+        const eventEnd = moment(
+          `${startingDate} - ${endingTime}`,
+          "YYYY-MM-DD - h:mm a"
+        ).format("YYYY-MM-DDTHH:mm:ss");
+
+        const eventDescription = $(element)
+          .find("p")
+          .text()
+          .replace(/\s+/g, " ");
+
+        // Logging to check extracted data
+        console.log("Event Header:", eventHeader);
+        console.log("Event Start:", eventStart);
+        console.log("Event End:", eventEnd);
+        console.log("Event Description:", eventDescription);
+
+        // Create an object for the event
+        const CEFB_CalendarBlock = {
+          source: "cefb",
+          title: eventHeader,
+          start: eventStart,
+          end: eventEnd,
+          description: eventDescription,
+        };
+
+        // Add the event object to an array
+        CEFB_events.push(CEFB_CalendarBlock);
+      }
+    );
+
+    // Logging to check the scraped events
+    console.log("CEFB Events:", CEFB_events);
+
+    // Insert all events into the database
+    for (const event of CEFB_events) {
+      await pool.query(
+        `INSERT INTO "calendar" ("source", "title", "start", "end", "description")
+            VALUES ($1, $2, $3, $4, $5);`,
+        [event.source, event.title, event.start, event.end, event.description]
+      );
+    }
+    console.log("Successfully inserted CEFB events into database");
+
+    res.sendStatus(200);
+  } catch (error) {
+    console.error("Error scraping CEFB calendar:", error);
+    res.sendStatus(500);
+  }
+});
 
 
 module.exports = router;
