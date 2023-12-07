a242712fc0c9a4ff77a1866a60f044536fe3e115
SRJ-91 <Sawyerjen@hotmail.com>
2023-12-07 16:08:59 -0600

    quick fixes
    Log Out button now text.
    
    Nav button closed

--- src/components/Shared/LogOutButton/LogOutButton.jsx
+++ src/components/Shared/LogOutButton/LogOutButton.jsx
@@ -16,7 +16,7 @@ function LogOutButton(props) {
 
   return (
     <Button
-      variant='contained'
+      variant='text'
       // This Button shows up in multiple locations and is styled differently
       // because it's styled differently depending on where it is used, the className
       // is passed to it from it's parents through React props
